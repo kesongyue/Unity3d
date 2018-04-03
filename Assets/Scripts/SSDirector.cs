862e809b9e98c4e9ba044184efe9b146d299b29c
kesongyue <13018597516@163.com>
2018-04-03 23:23:04 +0800

    submit

---
+++ Assets/Scripts/SSDirector.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MyGame;
+
+namespace MyGame{
+	public enum State{ BOATONSTART ,BOATMOVINGSTOE,BOATMOVINGETOS,BOATONEND,WIN,LOSE};
+
+	public interface IUserActions{
+		void priestStartOnBoat ();
+		void priestEndOnBoat ();
+		void devilStartOnBoat ();
+		void devilEndBoat ();
+		void moveBoat ();
+		void offBoatLeft ();
+		void offBoatRight ();
+		void Restart ();
+	}
+
+	public class GameScenceController: System.Object,IUserActions{
+		private static GameScenceController _instance;
+		private SSDirector _ssdirector;
+		private GenGameObjects _genGameObject;
+		public State state = State.BOATONSTART;
+
+		public static GameScenceController getInstance(){
+			if(_instance == null){
+				_instance = new GameScenceController ();
+			}
+			return _instance;
+		}
+
+		public SSDirector getSSDirector(){
+			return _ssdirector;
+		}
+
+		internal void  setSSDirector(SSDirector ssd){
+			if(_ssdirector == null){
+				_ssdirector = ssd;
+			}
+		}
+
+		public GenGameObjects getGenGameObject(){
+			return _genGameObject;
+		}
+
+		internal void setGenGameObject(GenGameObjects gen){
+			if(_genGameObject == null){
+				_genGameObject = gen;
+			}
+		}
+
+		public void priestStartOnBoat(){
+			_genGameObject.priestStartOnBoat ();
+		}
+			
+		public void priestEndOnBoat (){
+			_genGameObject.priestEndOnBoat ();
+		}
+
+		public void devilStartOnBoat (){
+			_genGameObject.devilStartOnBoat ();
+		}
+
+		public void devilEndBoat (){
+			_genGameObject.devilEndBoat ();
+		}
+
+		public void moveBoat (){
+			_genGameObject.moveBoat ();
+		}
+
+		public void offBoatLeft (){
+			_genGameObject.getOffBoat (0);
+		}
+
+		public void offBoatRight (){
+			_genGameObject.getOffBoat (1);
+		}
+
+		public void clickWhichOne(GameObject obj){
+			_genGameObject.clickWhichOne (obj);
+		}
+
+		public void Restart (){
+			Application.LoadLevel (Application.loadedLevelName);
+			state = State.BOATONSTART;
+		}
+	}
+
+}
+public class SSDirector : MonoBehaviour {
+
+	public string gameName;
+	// Use this for initialization
+	void Start () {
+		GameScenceController my = GameScenceController.getInstance ();
+		my.setSSDirector (this);
+		gameName = "Priests and Devils!";
+	}
+	
+
+}
+		
---
+++ Assets/Scripts/UserInterface.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MyGame;
+
+public class UserInterface : MonoBehaviour {
+	GameScenceController my;
+	IUserActions action;
+
+	// Use this for initialization
+	void Start () {
+		my = GameScenceController.getInstance ();
+		action = GameScenceController.getInstance () as IUserActions;
+	}
+	
+	void Update(){
+		if(Input.GetMouseButtonDown(0)){
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hit;
+
+			if(Physics.Raycast(ray,out hit)){
+				if(hit.transform.tag=="Devil" || hit.transform.tag=="Priest"){
+					my.clickWhichOne (hit.collider.gameObject);
+					//Debug.Log ("P and D");
+				}
+				else if(hit.transform.tag == "Boat"){
+					my.moveBoat ();
+					//Debug.Log ("Boat");
+				}
+			}
+		}
+	}
+
+	void OnGUI(){
+		if(GUI.Button(new Rect(0,0,80,80),"Restart")){
+			action.Restart();
+		}
+		if(my.state == State.WIN){
+			GUI.Label(new Rect(Screen.width/6,Screen.height/6,80,80),"WIN!");
+		}
+		else if(my.state == State.LOSE){
+			GUI.Label(new Rect(Screen.width/6,Screen.height/6,80,80),"LOSE!");
+		}
+	}
+}
